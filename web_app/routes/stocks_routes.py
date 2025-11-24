0d669b415e9b8b2481bc810d1414c177ebf5f5a8
ht462 <142940363+ht462@users.noreply.github.com>
2025-11-24 11:58:31 -0500

    Web update (#13)
    * testing
    
    * Flask Addition
    
    * RPS game web page
    
    * RPS game function addition
    
    * nav bar
    
    * stocks dashboard addition
    
    * web update

--- web_app/routes/stocks_routes.py
+++ web_app/routes/stocks_routes.py
@@ -2,7 +2,7 @@
 
 from flask import Blueprint, request, render_template, redirect, flash
 
-from apps.stocks import fetch_stocks_data, format_usd
+from apps.stocks import fetch_stocks_csv, format_usd
 
 stocks_routes = Blueprint("stocks_routes", __name__)
 
@@ -27,7 +27,7 @@ def stocks_dashboard():
     symbol = request_data.get("symbol") or "NFLX"
 
     try:
-        df = fetch_stocks_data(symbol=symbol)
+        df = fetch_stocks_csv(symbol=symbol)
         latest_close_usd = format_usd(df.iloc[0]["adjusted_close"])
         latest_date = df.iloc[0]["timestamp"]
         data = df.to_dict("records")
@@ -59,7 +59,7 @@ def stocks_api():
     symbol = url_params.get("symbol") or "NFLX"
 
     try:
-        df = fetch_stocks_data(symbol=symbol)
+        df = fetch_stocks_csv(symbol=symbol)
         data = df.to_dict("records")
         return {"symbol": symbol, "data": data }
     except Exception as err:
